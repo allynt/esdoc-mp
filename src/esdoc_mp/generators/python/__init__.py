9ee2cf6a51b629f792c8f380d3f4a8913ed2a231
Mark Morgan <momipsl@ipsl.jussieu.fr>
2013-04-02 12:04:40 +0200

    Repo initialisation after name refactor

---
+++ src/esdoc_mp/generators/python/__init__.py
@@ -0,0 +1,16 @@
+"""
+.. module:: esdoc_mp.generators.python.__init__.py
+   :copyright: Copyright "Feb 7, 2013", Earth System Documentation
+   :license: GPL/CeCIL
+   :platform: Unix, Windows
+   :synopsis: Package initialisor.
+
+.. moduleauthor:: Mark Conway-Greenslade (formerly Morgan) <momipsl@ipsl.jussieu.fr>
+
+
+"""
+
+from esdoc_mp.generators.python.root_generator import RootGenerator
+from esdoc_mp.generators.python.types_generator import TypesGenerator
+from esdoc_mp.generators.python.decoding_generator import DecodingGenerator
+from esdoc_mp.generators.python.validation_generator import ValidationGenerator
---
+++ src/esdoc_mp/ontology/__init__.py
@@ -0,0 +1,21 @@
+"""
+.. module:: esdoc_mp.ontology.__init__.py
+   :copyright: Copyright "Feb 7, 2013", Earth System Documentation
+   :license: GPL/CeCIL
+   :platform: Unix, Windows
+   :synopsis: esdoc_mp.ontology package initialisor.
+
+.. moduleauthor:: Mark Conway-Greenslade (formerly Morgan) <momipsl@ipsl.jussieu.fr>
+
+
+"""
+
+# Module imports.
+from esdoc_mp.ontology.class_ import Class
+from esdoc_mp.ontology.decoding import Decoding
+from esdoc_mp.ontology.enum import Enum
+from esdoc_mp.ontology.enum_member import EnumMember
+from esdoc_mp.ontology.ontology import Ontology
+from esdoc_mp.ontology.package import Package
+from esdoc_mp.ontology.property import Property
+from esdoc_mp.ontology.type import Type
